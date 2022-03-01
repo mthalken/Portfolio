d4186215314f06ff2ee57950f48a8528c48e9778
Mike Thalken <mthalken53@gmail.com>
2022-02-28 18:16:09 -0800

    Editing layout and adjusting images.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,11 @@ function showImgContent(e) {
     }
 };
 
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
 document.addEventListener('mousemove', showImgContent);
